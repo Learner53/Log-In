3a40c614c5ab6a511a037b65f1c76026f974e9f1
Abhishek Kumar <abhishek@abhi.k>
2019-10-27 20:20:35 +0530

    initial commit

---
+++ src/app/login.service.ts
@@ -0,0 +1,8 @@
+import { HttpClient } from '@angular/common/http';
+// import { loginData } from './app.component'
+
+export class LoginService {
+    createUser( httpClient:HttpClient){
+        // return this.client.post(`${DATA_ACCESS_PREFIX}`, traveller);
+      }
+}
